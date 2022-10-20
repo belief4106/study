f6b11291eb0625c18676755e43bd58227d4ca344
belief <17633564140@163.com>
2022-10-20 23:30:54 +0800

    user提交测试

---
+++ users/src/main/java/com/study/zyh/user/service/UserService.java
@@ -0,0 +1,4 @@
+package com.study.zyh.user.service;
+
+public interface UserService {
+}
---
+++ users/src/main/java/com/study/zyh/user/service/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.study.zyh.user.service;
+
+public class UserServiceImpl implements UserService {
+}
